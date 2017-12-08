9906804619dd7c2fc79344ed3a8bc04ee9bbf1f6
Jon Peirce <jon.peirce@gmail.com>
2017-12-08 12:03:05 +0000

    FF: move StrictVersion to use parse_version
    Some versions of openpyxl not always strictversion compatible
    e.g. 2.3.0-b2 on Travis

--- psychopy/data.py
+++ psychopy/data.py
@@ -23,12 +23,12 @@ import weakref
 import re
 import warnings
 import collections
-from distutils.version import StrictVersion
+from pkg_resources import parse_version
 
 try:
     # import openpyxl
     import openpyxl
-    if StrictVersion(openpyxl.__version__) >= StrictVersion('2.4.0'):
+    if parse_version(openpyxl.__version__) >= parse_version('2.4.0'):
         # openpyxl moved get_column_letter to utils.cell
         from openpyxl.utils.cell import get_column_letter
     else:
